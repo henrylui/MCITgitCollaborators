631d05a66817c5b76ad00e50b4d6bef3c8447499
Henry Lui <henrykinsunl@gmail.com>
2019-04-11 09:37:38 -0500

    Create AdditionCalculator.java

---
+++ AdditionCalculator.java
@@ -0,0 +1,15 @@
+
+public class AdditionCalculator {
+
+
+	public int addIntegersWithTwist(int a,int b) {
+		//Add integers along with a random selected number (See line 8)
+		
+		int twist = 3;
+		
+		int c = a + b + twist;
+		
+		return c;
+	}
+	
+}
